97b37c95d336704f75c6bbea2a08f39ff6368e29
Michael Boyd <boyder7@live.ca>
2016-01-24 09:42:45 -0500

    final

--- app/src/main/java/com/macadamian/smartpantry/ui/activities/Shop.java
+++ app/src/main/java/com/macadamian/smartpantry/ui/activities/Shop.java
@@ -1,18 +1,33 @@
 package com.macadamian.smartpantry.ui.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 import com.macadamian.smartpantry.R;
 
 public class Shop extends Activity {
-    String products[] = {"Milk", "Cereal", "Bread" , "Chips", "Chicken", "Apples", "Ham", "Fish"};
-    int aisles[] = {1,2,2,2,3,4,3,5};
-    String sections[] = {"Dairy, Grains, Grains, Grains, Meat, Produce, Meat, Seafood"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-    }
 
+        Button button1 = (Button)findViewById(R.id.deli);
+
+        button1.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        System.out.print("yolo");
+                    }
+                }
+        );
+    }
 }

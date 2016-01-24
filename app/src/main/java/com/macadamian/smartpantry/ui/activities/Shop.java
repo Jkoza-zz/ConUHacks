0500f4f2939cd0dea98e25a3eec12b13f3a5dfc9
Joshua Koza <jlkoza@uwaterloo.ca>
2016-01-23 19:56:37 -0800

    a

--- app/src/main/java/com/macadamian/smartpantry/ui/activities/Shop.java
+++ app/src/main/java/com/macadamian/smartpantry/ui/activities/Shop.java
@@ -6,7 +6,9 @@ import android.app.Activity;
 import com.macadamian.smartpantry.R;
 
 public class Shop extends Activity {
-
+    String products[] = {"Milk", "Cereal", "Bread" , "Chips", "Chicken", "Apples", "Ham", "Fish"};
+    int aisles[] = {1,2,2,2,3,4,3,5};
+    String sections[] = {"Dairy, Grains, Grains, Grains, Meat, Produce, Meat, Seafood"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
